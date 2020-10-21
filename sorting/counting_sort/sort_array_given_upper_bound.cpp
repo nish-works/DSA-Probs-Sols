75823d437d38df5ba8a6534367009bae2c57c84e
nishitagiduturi <nishitaoct26@gmail.com>
2020-10-21 23:29:20 +0530

    Counting sort for non negative integers

---
+++ sorting/counting_sort/sort_array_given_upper_bound.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// For non negative integers
+// TC: O(n+k) AS: O(n+k)
+void countingSort(vector<int> &arr, int n, int k)
+{
+    vector<int> count(n, 0), output(n, 0);
+    for (int i = 0; i < n; ++i)
+        ++count[arr[i]];
+    for (int i = 1; i < n; ++i)
+        count[i] += count[i - 1];
+    for (int i = 0; i < n; ++i)
+        count[i] = count[i] - 1;
+    for (int i = 0; i < n; ++i)
+    {
+        output[count[arr[i]]] = arr[i];
+        --count[arr[i]];
+    }
+    arr = output;
+}
+
+int main()
+{
+    int n, k;
+    cin >> n >> k;
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    countingSort(arr, n, k);
+    for (int x : arr)
+        cout << x << " ";
+    return 0;
+}
